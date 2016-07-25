5221a172cf2cd30b7bda6910451eaa3d073cea6f
Robert <grimes@rgconsulting.com>
2016-07-25 07:24:48 -0700

    send mm.log

--- release/update.sh
+++ release/update.sh
@@ -3,4 +3,15 @@ echo "clear git lock"
 rm .git/*.lock
 echo "cleared"
 
+MUMBLE='mumble:'
+CAPLINK='/home/pi/CAPLink'
+
+SERIAL="$(cat /proc/cpuinfo | grep Serial | cut -d ':' -f 2)"
+SERIAL="$(echo "${SERIAL}" | sed -e 's/^[[:space:]]*//')"
+
+cd $CAPLINK
+#send the mm.log even though it might get overwritten below
+echo "send mm.log"
+curl -T mm.log -u caplink:mumble ftp://caplink.azwg.org/CAPLink/$SERIAL/
+
 echo "nothing to do"
