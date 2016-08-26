a0e87c5eef8c316d8b010eb373a96c6c3d4398ee
Robert <grimes@rgconsulting.com>
2016-08-26 03:20:36 -0700

    Update

--- release/update.sh
+++ release/update.sh
@@ -18,7 +18,7 @@ curl -T m.log -u caplink:mumble ftp://caplink.azwg.org/CAPLink/$SERIAL/
 
 echo "send start.sh"
 curl -T start.sh -u caplink:mumble ftp://caplink.azwg.org/CAPLink/$SERIAL/
-
+ls -l  >> /home/pi/CAPLink/start.log
 
 stat /home/pi/CAPLink/release/mumble.sh >> /home/pi/CAPLink/start.log
 cat /home/pi/CAPLink/release/mumble.sh >> /home/pi/CAPLink/start.log
